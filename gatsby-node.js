7ad02a12ab38e0d5089bfb4bb57b8edddfc08a08
Bardia Daniari <bardiadaniari@gmail.com>
2020-08-16 18:11:20 +0200

    addition of markdown file & tutorial.js component

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path');
+
+module.exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions
+    // Transform the new node here and create a new node or
+    // create a new node field.
+
+    if(node.internal.type === 'MarkdownRemark'){
+      const slug = path.basename(node.fileAbsolutePath, '.md')
+      
+      
+      createNodeField({
+        node,
+        name: 'slug',
+        value: slug
+      })
+
+    }
+  }
+
+  module.exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+  
+  }
---
+++ src/templates/tutorial.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Tutorial = () => {
+    return (
+        <div>this is the tutorial template</div>
+    )
+}
+
+export default Tutorial;
